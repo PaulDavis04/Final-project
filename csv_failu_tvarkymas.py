c7eada156e8d90a5bfbaa410a6174c121db65a20
PaulDavis04 <149395819+PaulDavis04@users.noreply.github.com>
2023-11-13 18:01:22 +0200

    Nuskreipinta_Lentelė
    Nususkreipinta lentelė, išdropintos eilutės ir stulpeliai

--- csv_failu_tvarkymas.py
+++ csv_failu_tvarkymas.py
@@ -1,17 +1,25 @@
 import pandas as pd
 
-df = pd.read_csv(r"C:\Users\pauli\OneDrive\Dokumentai\GitHub\Final-project\LIGOS.csv")
-#
-# df = df.rename(columns={"Suply":"Supply"})
+df = pd.read_csv(r"scraped_data.csv.")
+df = df.drop([0,1,2,3,4,5,6])
 
-df = df.drop([0,2,3,4,5,6,8])
-df.set_index("column_name", inplace=True)
-df = df.drop(labels=your_dict.keys(), inplace=True)
+# df = df.drop(columns=[1])
+
+df = df.iloc[:, 0:5]
 
 
-# df["Name"] = df["Name"].str.lstrip("")
+
+
+# df.set_index("data", inplace=True)
+
+# df = df.drop(labels=[], inplace=True)
+
+print(df)
+# df["C2"] = df["C2"].str.replace('\xa0', '')
+
+# df["data"] = df["data"].str.lstrip("")
 # df["Price"] = df["Price"].str.lstrip("€")
 # df["Market cap"] = df["Market cap"].str.lstrip("€")
 # df["Volume (24)"] = df["Volume (24)"].str.lstrip("€")
 
-df.to_csv("LIGOS_redaguotas1.csv", index=False)
+df.to_csv("LIGOS.csv")
