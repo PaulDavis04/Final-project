89e279eabe234a59485d32fe52620a19cc338b8f
PaulDavis04 <149395819+PaulDavis04@users.noreply.github.com>
2023-11-14 11:57:52 +0200

    Lentelė

--- pavadinimai_tvarkymas.py
+++ pavadinimai_tvarkymas.py
@@ -1,11 +1,17 @@
 import psycopg2
 from psycopg2 import sql
 import pandas as pd
-
 df = pd.read_csv(r"skreipinti_pavadinimai.csv.")
-
+df = df.rename({"0": "Ligos", "1": "Tuscia"}, axis='columns')
+df.set_index('Ligos', inplace = True)
 df.to_csv("pavadinimai_naujas.csv", index=True)
-
 # df = df.rename({"Vyrai ir moterys": "Tam tikros infekcinės ir parazitų sukeliamos ligos"}, axis='columns')
 # df['C2'] = df['C2'].replace(['Vyrai ir moterys'], 'Tam tikros infekcinės ir parazitų sukeliamos ligos')
-df.to_csv("skreipinti_pavadinimai_naujas.csv", index=True)
+# df.to_csv("skreipinti_pavadinimai_naujas.csv", index=True)
+
+
+
+
+
+
+
