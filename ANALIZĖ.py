6d7523e74feb008c31a4593b53227031d52ea855
PaulDavis04 <149395819+PaulDavis04@users.noreply.github.com>
2023-11-14 15:23:34 +0200

    Analize

---
+++ ANALIZĖ.py
@@ -0,0 +1,9 @@
+import pandas as pd
+from postgres import work_with_database
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.linear_model import LinearRegression
+from datetime import datetime
+
+
