7459d54d03b0dbdc61fefbde3cde26b6fe143602
PaulDavis04 <149395819+PaulDavis04@users.noreply.github.com>
2023-11-14 14:19:30 +0200

    Lentelės padalinimas

--- csv_failu_tvarkymas.py
+++ csv_failu_tvarkymas.py
@@ -1,42 +1,13 @@
 import pandas as pd
 
-df1 = pd.read_csv("pavadinimai_naujas.csv")
-df2 = pd.read_csv("naujas.csv")
-df3 = pd.concat([df1, df2], axis=1)
+df = pd.read_csv(r"bendra_lentele.csv")
+
+df_Vyrai_Moterys = df.iloc[:37,:]
+df_Vyrai = df.iloc[37:74,:]
+df_Moterys = df.iloc[74:111,:]
 
 
-print(df3)
-df3.to_csv("LENTELĖ")
-
-# df3 = df3.drop(columns=[0])
-
-
-# df3.to_csv("Ligos index")
-
-# df = pd.read_csv(r"scraped_data.csv.")
-# df = df.drop([0,1,2,3,4,5,6])
-#
-# # df = df.drop(columns=[1])
-#
-# df = df.iloc[:, 0:5]
-# df = df.set_index("data", inplace=True)
-# df1 = pd.read_csv(r"skreipinti_pavadinimai.csv")
-# df1 = df.set_index("data", inplace=True)
-# df2 = pd.concat([df,df1], axis=1)
-# print(df2)
-
-# df2.to_csv("Lentlė.csv")
-
-# df.set_index("data", inplace=True)
-
-# df = df.drop(labels=[], inplace=True)
-
-# print(df)
-# df["C2"] = df["C2"].str.replace('\xa0', '')
-
-# df["data"] = df["data"].str.lstrip("")
-# df["Price"] = df["Price"].str.lstrip("€")
-# df["Market cap"] = df["Market cap"].str.lstrip("€")
-# df["Volume (24)"] = df["Volume (24)"].str.lstrip("€")
-
-# df.to_csv("LIGOS.csv")
+print("Shape of new dataframes - {} , {}".format(df_Vyrai_Moterys.shape, df_Vyrai.shape))
+df_Vyrai_Moterys.to_csv("Vyrai_Moterys.csv", index=False)
+df_Vyrai.to_csv("Vyrai.csv", index=False)
+df_Moterys.to_csv("Moterys.csv", index=False)
--- pavadinimai_tvarkymas.py
+++ pavadinimai_tvarkymas.py
@@ -5,9 +5,22 @@ df = pd.read_csv(r"skreipinti_pavadinimai.csv.")
 df = df.rename({"0": "Ligos", "1": "Tuscia"}, axis='columns')
 df.set_index('Ligos', inplace = True)
 df.to_csv("pavadinimai_naujas.csv", index=True)
-# df = df.rename({"Vyrai ir moterys": "Tam tikros infekcinės ir parazitų sukeliamos ligos"}, axis='columns')
-# df['C2'] = df['C2'].replace(['Vyrai ir moterys'], 'Tam tikros infekcinės ir parazitų sukeliamos ligos')
-# df.to_csv("skreipinti_pavadinimai_naujas.csv", index=True)
+
+df1 = pd.read_csv("pavadinimai_naujas.csv")
+df2 = pd.read_csv("naujas.csv")
+df3 = pd.concat([df1, df2], axis=1)
+
+
+df3['Ligos'] = df3['Ligos'].replace(['Vyrai ir moterys', 'Vyrai', 'Moterys'], ['Tam tikros infekcinės ir parazitų sukeliamos ligos',
+                                                                            'Tam tikros infekcinės ir parazitų sukeliamos ligos',
+                                                                            'Tam tikros infekcinės ir parazitų '
+                                                                            'sukeliamos ligos'])
+df3.set_index('Ligos', inplace = True)
+df3.drop(df.iloc[:, 1:2], inplace=True, axis=1)
+print(df3)
+df3.to_csv("bendra_lentele.csv", index=True)
+
+
 
 
 
