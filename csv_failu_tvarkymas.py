783a763f3710dfd7f631a863a85d293e217811ca
PaulDavis04 <149395819+PaulDavis04@users.noreply.github.com>
2023-11-14 11:33:33 +0200

    Antra diena

--- csv_failu_tvarkymas.py
+++ csv_failu_tvarkymas.py
@@ -1,20 +1,33 @@
 import pandas as pd
 
-df = pd.read_csv(r"scraped_data.csv.")
-df = df.drop([0,1,2,3,4,5,6])
+df3 = pd.read_csv("LIGOS.csv")
+df3.set_index('2018', inplace = True)
+df3 = df3.loc[:, ~df3.columns.str.contains('^Unnamed')]
+print(df3)
+df3.to_csv("Ligos index", index=False)
 
-# df = df.drop(columns=[1])
+# df3 = df3.drop(columns=[0])
+# df3.to_csv("Ligos index")
 
-df = df.iloc[:, 0:5]
+# df = pd.read_csv(r"scraped_data.csv.")
+# df = df.drop([0,1,2,3,4,5,6])
+#
+# # df = df.drop(columns=[1])
+#
+# df = df.iloc[:, 0:5]
+# df = df.set_index("data", inplace=True)
+# df1 = pd.read_csv(r"skreipinti_pavadinimai.csv")
+# df1 = df.set_index("data", inplace=True)
+# df2 = pd.concat([df,df1], axis=1)
+# print(df2)
 
-
-
+# df2.to_csv("Lentlė.csv")
 
 # df.set_index("data", inplace=True)
 
 # df = df.drop(labels=[], inplace=True)
 
-print(df)
+# print(df)
 # df["C2"] = df["C2"].str.replace('\xa0', '')
 
 # df["data"] = df["data"].str.lstrip("")
@@ -22,4 +35,4 @@ print(df)
 # df["Market cap"] = df["Market cap"].str.lstrip("€")
 # df["Volume (24)"] = df["Volume (24)"].str.lstrip("€")
 
-df.to_csv("LIGOS.csv")
+# df.to_csv("LIGOS.csv")
